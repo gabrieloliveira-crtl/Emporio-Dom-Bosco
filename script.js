c885fa6095e3beed512b9ad0e8ab820fa8b7947f
Gabriel Oliveira <gabrieloliverasantos162@gmail.com>
2024-10-11 19:35:22 -0300

    gujtgj

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ hamburgerButton.addEventListener("click", function(){
     mobileMenu.classList.add("flex");
     //criamos uma funçao que a ouvir o click do mouse, ele adicione uma class, que esta fazendo nossa caixa(div) aparecer, ou seja, ficar visivel.
 });
+
 //criamos uma funçao que a ouvir o click do mouse, ele REMOVE uma class, que esta fazendo nossa caixa(DIv)desaparecer,ou seja, ficar invisivel.
 closeButton.addEventListener("click", function(){
     mobileMenu.classList.remove("flex");
